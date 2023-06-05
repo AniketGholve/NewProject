c46111c97950a574337cefcb5fa22a91906f9a91
Aniket Gholve <aniketgholve02@gmail.com>
2023-06-05 23:41:45 +0530

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,71 +3,63 @@ import { NavLink } from 'react-router-dom';
 import './style.css'
 function Header() {
     let [userRole, setUserRole] = useState("");
-    
+    let [activeTab,setActiveTab]=useState("");
     useEffect(() => {
         setUserRole(sessionStorage.getItem("userRole"))
     },[])
-    
+    const toggleActive=(tab)=>{
+        setActiveTab(tab);
+    }
     return (
         <>
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <NavLink activeClassName="active_class" className="navbar-brand" to="#!">Inventory Management System</NavLink>
+                    <a className="navbar-brand">Inventory Management System</a>
                     <div className="collapse navbar-collapse">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             {
-                                userRole === "" && <>
-                                    <li className="nav-item" >
-                                        <NavLink className="nav-link" href="#!">Login</NavLink>
-                                    </li>
-                                    <li className="nav-item">
-                                        <NavLink className="nav-link" href="#!register">Register</NavLink>
-                                    </li>
-                                </>
-                            }
-                            {
                                 userRole === "CLP" && <>
                                     <li className="nav-item">
-                                        <NavLink className="nav-link" href="#!/clp_users">Home</NavLink>
+                                        <NavLink onClick={()=>toggleActive("home")} className={activeTab === 'home' ? 'nav-link active_class':'nav-link' }>Home</NavLink>
                                     </li>
                                     <li className="nav-item">
-                                        <NavLink className="nav-link" href="#!/patient">Patients</NavLink>
+                                        <NavLink onClick={()=>toggleActive("patient")} className={activeTab === 'patient' ? 'nav-link active_class':'nav-link' }>Patients</NavLink>
                                     </li>
                                     <li className="nav-item">
-                                        <NavLink className="nav-link" href="#!inventory">Inventory</NavLink>
+                                        <NavLink onClick={()=>toggleActive("inventory")} className={activeTab === 'inventory' ? 'nav-link active_class':'nav-link' }>Inventory</NavLink>
                                     </li>
                                     <li className="nav-item">
-                                        <NavLink className="nav-link" href="#!/administrator">Administrator</NavLink>
+                                        <NavLink onClick={()=>toggleActive("administrator")} className={activeTab === 'administrator' ? 'nav-link active_class':'nav-link' }>Administrator</NavLink>
                                     </li>
                                     <li className="nav-item">
-                                        <NavLink className="nav-link" href="#!/reports">Reports</NavLink>
+                                        <NavLink onClick={()=>toggleActive("report")} className={activeTab === 'report' ? 'nav-link active_class':'nav-link' }>Reports</NavLink>
                                     </li>
                                 </>
                             }
                             {userRole === "ALP" && <>
                                 <li className="nav-item">
-                                    <NavLink className="nav-link" href="#!clinics">Clinics</NavLink>
+                                    <NavLink onClick={()=>toggleActive("clinic")} className={activeTab === 'clinic' ? 'nav-link active_class':'nav-link'}>Clinics</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink className="nav-link" href="#!reporting">Reporting</NavLink>
+                                    <NavLink onClick={()=>toggleActive("reporting")} className={activeTab === 'reporting' ? 'nav-link active_class':'nav-link' }>Reporting</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink className="nav-link" href="#!/ordersInfo">Ordering</NavLink>
+                                    <NavLink onClick={()=>toggleActive("ordering")} className={activeTab === 'ordering' ? 'nav-link active_class':'nav-link' }>Ordering</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink className="nav-link" href="#!/shipping">Shipping</NavLink>
+                                    <NavLink onClick={()=>toggleActive("shipping")} className={activeTab === 'shipping' ? 'nav-link active_class':'nav-link' }>Shipping</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink className="nav-link" href="#!/setup">Setup</NavLink>
+                                    <NavLink onClick={()=>toggleActive("setup")} className={activeTab === 'setup' ? 'nav-link active_class':'nav-link' }>Setup</NavLink>
                                 </li>
                             </>}
                             {
                                 userRole === "ELP" && <>
                                     <li className="nav-item">
-                                        <a className="nav-link" href="#!/elp_users">Success Orders</a>
+                                        <NavLink onClick={()=>toggleActive("successOrder")} className={activeTab === 'successOrder' ? 'nav-link active_class':'nav-link' }>Success Orders</NavLink>
                                     </li>
                                     <li className="nav-item">
-                                        <a className="nav-link" href="#!/error_orders">Error Orders</a>
+                                        <NavLink onClick={()=>toggleActive("errorOrder")} className={activeTab === 'errorOrder' ? 'nav-link active_class':'nav-link' }>Error Orders</NavLink>
                                     </li>
                                 </>
                             }
